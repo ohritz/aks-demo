ca6ea7db9b304b5dbeeac0aececdbd9860170606
Sohan Fernando <sohan@graceful.nu>
2023-07-03 12:32:48 +0200

    fixed products api to return ids

--- src/products-api/route-handlers/get-products-by-category.js
+++ src/products-api/route-handlers/get-products-by-category.js
@@ -1,13 +1,13 @@
-const { getProductModel} = require("../dal");
+const { getProductModel } = require("../dal");
 
 async function getProductsByCategory(request, h) {
-    const {categories}  = request.query;
-    const query = {
-        categories
-    };
-    const Products = await getProductModel();
-    const products = await Products.search(query);
-    return products;
+  const { categories } = request.query;
+  const query = {
+    categories,
+  };
+  const Products = await getProductModel();
+  const products = await Products.search(query);
+  return products.map((p) => p.toJSON());
 }
 
 exports.getProductsByCategory = getProductsByCategory;
