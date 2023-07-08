bca161c99db22f1c57cf1d3619d2afc4fde42c61
Sohan Fernando <sohan@graceful.nu>
2023-07-08 22:35:45 +0200

    added helper script to set dns label

---
+++ infrastructure/scripts/set-dns-label.sh
@@ -0,0 +1,17 @@
+
+# https://learn.microsoft.com/en-us/azure/aks/ingress-tls?tabs=azure-cli
+
+# Public IP address of your ingress controller
+IP="MY_EXTERNAL_IP"
+
+# Name to associate with public IP address
+DNSLABEL="cb-demo-2023"
+
+# Get the resource-id of the public IP
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public IP address with DNS name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSLABEL
+
+# Display the FQDN
+az network public-ip show --ids $PUBLICIPID --query "[dnsSettings.fqdn]" --output tsv
