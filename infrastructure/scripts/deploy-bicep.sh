fdb08255f64d0f0df8b228ed44680bb8fb4e2e8e
Sohan Fernando <sohan@graceful.nu>
2023-08-27 22:28:28 +0200

    deploy script to print output to file

--- infrastructure/scripts/deploy-bicep.sh
+++ infrastructure/scripts/deploy-bicep.sh
@@ -5,10 +5,12 @@ location="$(cat ./config.json | jq -r '.location')"
 
 pushd ../bicep
 
-az deployment sub create \
---name $deploymentName \
---location $location \
---template-file main.bicep \
---parameters @main.parameters.json
+output=$(az deployment sub create \
+  --name $deploymentName \
+  --location $location \
+  --template-file main.bicep \
+--parameters @main.parameters.json)
 
 popd
+
+echo $output | jq > output.json
