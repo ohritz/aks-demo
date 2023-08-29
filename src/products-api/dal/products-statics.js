ca4b496a1637db80fe0293c13d92057568d6e4e4
Sohan Fernando <sohan@graceful.nu>
2023-08-29 08:52:06 +0200

    Loggin in products api

--- src/products-api/dal/products-statics.js
+++ src/products-api/dal/products-statics.js
@@ -1,3 +1,5 @@
+const { logger } = require("../app-config/logger");
+
 const ProductStatics = {
   search: async function (queryPararms) {
     const { categories } = queryPararms;
@@ -7,14 +9,21 @@ const ProductStatics = {
     }
 
     const products = await this.find(query);
+    logger.info(`found ${products.length} products`);
     return products;
   },
   getOneById: async function (id) {
     const product = await this.findOne({ _id: id });
+    if (!product) {
+      logger.info(`product ${id} not found`);
+      return null;
+    }
+    logger.info(`found product ${product._id}`);
     return product;
   },
   getCategories: async function () {
     const categories = await this.distinct("category");
+    logger.info(`found ${categories.length} categories`);
     return categories;
   },
 };
