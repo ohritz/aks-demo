8d944dfe4980915dd92adb44f126b77c58de73b8
Sohan Fernando <sohan@graceful.nu>
2023-08-29 09:01:11 +0200

    fixes in graphql-api

--- src/graphql-api/src/schema/categories/resolvers/Query/categories.ts
+++ src/graphql-api/src/schema/categories/resolvers/Query/categories.ts
@@ -1,9 +1,12 @@
 import type { QueryResolvers } from "./../../../types.generated";
 import { fetchCategories } from "../../../../clients/products-api";
+import { logger } from "../../../../config/logger";
 export const categories: NonNullable<QueryResolvers["categories"]> = async (
   _parent,
   _arg,
   _ctx
 ) => {
-  return fetchCategories();
+  const result = await fetchCategories();
+  logger.info("categories", { result });
+  return result;
 };
